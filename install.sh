89fbf494a63ad4bfd2d380f27c64d242ce860a43
brookehorizon <danieltashman17@gmail.com>
2022-07-25 20:11:05 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+sudo apt install tor -y
+sudo apt install proxychains -y
---
+++ pcs.py
@@ -0,0 +1,21 @@
+import os
+
+class PCS:
+    def startProxychains(self):
+        os.system('sudo service tor start')
+        
+    def text(self):
+        self.startProxychains()
+        username = os.popen('id -u -n').readline().strip()
+        Exit = False
+        
+        while not Exit:
+            c = input(f'\033[1;32;40m {username}>')
+            if c.lower() in ['exit', 'quit']:
+                Exit = True
+            os.system(f"proxychains {c}")
+
+
+if __name__ == '__main__':
+    PCS().text()
+
