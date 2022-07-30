d420954f2cb24a288214a9fb03a13e5092f951bb
brookehorizon <danieltashman17@gmail.com>
2022-07-31 00:45:48 +0300

    Added root view

--- core/gui/main/views.py
+++ core/gui/main/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
-import subprocess
+import subprocess, os
 
 def win(request):
     if request.method == 'POST':
         try:
+            if 'clear' in request.POST['command'].split():
+                return render(request, 'win.html')
+
+            if "cd" in request.POST['command'].split():
+                print(request.POST['command'].split()[1])
+                os.chdir(request.POST['command'].split()[1])
+                return render(request, 'win.html')
+
             output = subprocess.check_output(['proxychains'] + request.POST['command'].split()).decode()
             return render(request, 'win.html', {'out':output})
         except:
             pass
         
     return render(request, 'win.html')
+
+
+def root_privilege(request):
+    if request.method == 'POST':
+        try:
+            if 'clear' in request.POST['command'].split():
+                return render(request, 'win.html', {'root':True})
+
+            if "cd" in request.POST['command'].split():
+                print(request.POST['command'].split()[1])
+                os.chdir(request.POST['command'].split()[1])
+                return render(request, 'win.html', {'root':True})
+
+            output = subprocess.check_output(['sudo', 'proxychains'] + request.POST['command'].split()).decode()
+            return render(request, 'win.html', {'out':output, 'root':True})
+        except:
+            pass
+        
+    return render(request, 'win.html', {'root':True})
+
